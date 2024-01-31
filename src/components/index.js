8ee84aab20e5280430fc66e85fbb31181cd5eb23
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-01-31 17:37:45 -0300

    patris 1st

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Navbar from "./navbar/navbar";
+import Carousel from "./Carousel/Carousel";
+import Footer from "./Footer/Footer";
+
+
+export {
+    Navbar,
+    Carousel,
+    Footer
+};
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Navbar,
+    Carousel,
+    Footer
+} from '../../components/index';
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Carousel/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home;
