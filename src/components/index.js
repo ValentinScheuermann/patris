48ea34154285fb093156cfa489200498e2af3838
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-02-01 14:35:14 -0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Navbar from "./navbar/navbar";
+import Navbar from "./Navbar/Navbar";
 import Carousel from "./Carousel/Carousel";
 import Footer from "./Footer/Footer";
 import AboutUs from "./Aboutus/Aboutus";
