4a197efe90e51f328d91336856d64d804540ad70
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-02-01 14:36:58 -0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar/Navbar";
 import Carousel from "./Carousel/Carousel";
 import Footer from "./Footer/Footer";
-import AboutUs from "./Aboutus/Aboutus";
+import AboutUs from "./Aboutus/AboutUs";
 
 export {
     Navbar,
