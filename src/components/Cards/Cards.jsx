ab1d8e899c6bcd4515a8ac7aca20900cec92de92
Santiago Scheuermann <santisch1997@gmail.com>
2024-02-02 17:42:28 -0300

    Estilos cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from 'react';
+import padreyniño from '../../assets/CardPics/padreyniño.jpg';
 
 const Cards = () => {
   return (
-<div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title justify-center">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-center">
-      <button className="btn btn-primary">Listen</button>
+    <div className="card lg:card-side bg-base-100 shadow-xl w-4/5 mx-auto">
+      <figure className="lg:w-1/2">
+        <img src={padreyniño} alt="Album" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title justify-center text-3xl font-bold font-montserrat text-green-800">
+          En conjunto con los padres.
+        </h2>
+        <p className="text-base font-montserrat whitespace-normal max-w-[400px]">
+          {/* Utiliza "max-w-[400px]" para establecer un ancho máximo específico de 400 píxeles */}
+          Lorem ipsum dolor sit amet consectetur. Sed maecenas tempus quis elementum nunc integer accumsan. Dolor elit proin auctor vel non. Nec sed pharetra sapien euismod dignissim tortor. Nunc nisl fusce urna at mauris quis augue tincidunt. Condimentum sed donec rhoncus sapien non.
+        </p>
+        <div className="card-actions justify-center">
+          <button className="btn btn-primary">Ver más</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
