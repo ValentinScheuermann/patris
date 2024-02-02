6e44e20bdac73d4ab08a82096f144197828c6c3d
Santiago Scheuermann <santisch1997@gmail.com>
2024-02-02 17:58:51 -0300

    .

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import padreyniño from '../../assets/CardPics/padreyniño.jpg';
+import PropTypes from 'prop-types';
 
-const Cards = () => {
+const Cards = ({ cardData }) => {
+  const { image, title, description, buttonText } = cardData;
+
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl w-4/5 mx-auto">
       <figure className="lg:w-1/2">
-        <img src={padreyniño} alt="Album" />
+        <img src={image} alt="Album" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title justify-center text-3xl font-bold font-montserrat text-green-800">
-          En conjunto con los padres.
+        <h2 className="card-title justify-center text-4xl font-bold font-montserrat text-green-800">
+          {title}
         </h2>
-        <p className="text-base font-montserrat whitespace-normal max-w-[400px]">
-          {/* Utiliza "max-w-[400px]" para establecer un ancho máximo específico de 400 píxeles */}
-          Lorem ipsum dolor sit amet consectetur. Sed maecenas tempus quis elementum nunc integer accumsan. Dolor elit proin auctor vel non. Nec sed pharetra sapien euismod dignissim tortor. Nunc nisl fusce urna at mauris quis augue tincidunt. Condimentum sed donec rhoncus sapien non.
+        <p className="text-lg font-montserrat whitespace-normal max-w-[400px]">
+
+          {description}
         </p>
         <div className="card-actions justify-center">
-          <button className="btn btn-primary">Ver más</button>
+          <button className="btn btn-primary">{buttonText}</button>
         </div>
       </div>
     </div>
   );
+};
+
+Cards.propTypes = {
+  cardData: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    buttonText: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Cards;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React from 'react';
 import {
-    Navbar,
-    Carousel,
-    Footer,
-    Cards,
-    Divider,
-    Stats
+  Navbar,
+  Carousel,
+  Footer,
+  Cards,
+  Divider,
+  Stats
 } from '../../components/index';
 
+import padreyniñoImage from '../../assets/CardPics/padreyniño.jpg';
+import tedxImage from '../../assets/CardPics/tedx.jpg';
+
 const Home = () => {
+  const cardData1 = {
+    image: padreyniñoImage,
+    title: 'En conjunto con los padres.',
+    description: 'Lorem ipsum dolor sit amet consectetur. Sed maecenas tempus quis elementum nunc integer accumsan...',
+    buttonText: 'Ver más',
+  };
+
+  const cardData2 = {
+    image: tedxImage,
+    title: 'Construyendo el futuro.',
+    description: 'Lorem ipsum dolor sit amet consectetur. Sed maecenas tempus quis elementum nunc integer accumsan...',
+    buttonText: 'Ver más',
+  };
+
   return (
     <div>
-        <Navbar/>
-        <Divider/>
-        <Carousel/>
-        <Stats/>
-        <Divider/>
-        <Cards/>
-        <Divider/>
-        <Cards/>
-        <Footer/>
+      <Navbar />
+      <Divider />
+      <Carousel />
+      <Stats />
+      <Divider />
+      <Cards cardData={cardData1} />
+      <Divider />
+      <Cards cardData={cardData2} />
+      <Divider />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
