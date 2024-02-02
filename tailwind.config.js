684976ef8898324c2a4ca24b2b88090e42fdba6b
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-02-02 09:40:45 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,20 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('daisyui')],
   daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
+    themes: [
+      {
+        mytheme: {
+        "primary": "#2A6C3D",     
+        "secondary": "#2AA556", 
+        "accent": "#38bdf8",     
+        "neutral": "#F6EDD9",  
+        "base-100": "#F3FCF4",    
+        "info": "#4ab5ff",  
+        "success": "#00e38c", 
+        "warning": "#fef08a", 
+        "error": "#ff7781",
+        },
+      },
+    ],
   },
-
 };
