a123a99bec3a28e1f1580cbab9f6f01ab623e236
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-02-03 10:25:40 -0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,8 +7,10 @@ const Navbar = () => {
   <div className="navbar bg-[#2A6C3D] h-20">
   <div className="navbar-start">
     <div className="dropdown">
+    <div className="tooltip tooltip-bottom" data-tip="Menu">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+        </div>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
       <li><a href="/">Homepage</a></li>
