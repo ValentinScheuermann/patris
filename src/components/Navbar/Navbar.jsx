f45828784fa0ae6fd9a3da7a9c617514abd36e9d
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-02-05 17:24:08 -0300

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
 
     <div className="navbar bg-[#2A6C3D] h-20">
-    <div className="navbar-start">
+    <div className="navbar">
       <div className="dropdown">
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -22,7 +22,7 @@ const Navbar = () => {
           <li><a>Item 3</a></li>
         </ul>
       </div>
-      <a className="btn btn-ghost text-xl">Colegio Patris</a>
+      <a href='/' className="btn btn-ghost text-xl">Colegio Patris</a>
     </div>
     <div className="navbar-center hidden lg:flex">
       <ul className="menu menu-horizontal px-1">
@@ -44,7 +44,8 @@ const Navbar = () => {
               <li><a>Submenu 2</a></li>
             </ul>
           </details>
-          <li>
+        </li>
+        <li>
           <details>
             <summary>Comunidad</summary>
             <ul className="p-2">
@@ -52,7 +53,6 @@ const Navbar = () => {
               <li><a>Submenu 2</a></li>
             </ul>
           </details>
-        </li>
         </li>
         <li><a>Novedades</a></li>
       </ul>
--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -3,45 +3,27 @@ import logo from "../../assets/logo.png"
 
 const Stats = () => {
   return (
-    <div>
-        <div className="stats shadow">
+    <div className="stats stats-vertical lg:stats-horizontal shadow">
   
-  <div className="stat">
-    <div className="stat-figure text-primary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-      className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" 
-      strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-    </div>
-    <div className="stat-title">Mas de 500 Alumnos</div>
-    <div className="stat-value text-primary">500+</div>
-    <div className="stat-desc">Cupos limitados</div>
+  <div className="stat w-72">
+    <div className="stat-title">Downloads</div>
+    <div className="stat-value">31K</div>
+    <div className="stat-desc">Jan 1st - Feb 1st</div>
   </div>
   
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-      className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-    </div>
-    <div className="stat-title">Visitas</div>
-    <div className="stat-value text-secondary">2.6M</div>
-    <div className="stat-desc">21% mas que el mes pasado!</div>
+  <div className="stat w-72">
+    <div className="stat-title">New Users</div>
+    <div className="stat-value">4,200</div>
+    <div className="stat-desc">↗︎ 400 (22%)</div>
   </div>
   
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-      <div className="avatar online">
-        <div className="w-16 rounded">
-          <img src={logo} />
-        </div>
-      </div>
-    </div>
-    <div className="stat-value">+450</div>
-    <div className="stat-title">Mas de 450 egresados!</div>
-    <div className="stat-desc text-secondary">1998-2024</div>
+  <div className="stat w-72">
+    <div className="stat-title">New Registers</div>
+    <div className="stat-value">1,200</div>
+    <div className="stat-desc">↘︎ 90 (14%)</div>
   </div>
   
 </div>
-    </div>
   )
 }
 
